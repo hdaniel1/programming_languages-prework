706e168d494e1b4bd6779764fd30c52019a9d8b8
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-13 23:28:20 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,7 +1,7 @@
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |keys, values|
-    language.each do |values, types|
+    values.each do |values, types|
       new_hash[values] = types
       new_hash[values][:style] = []
     end
