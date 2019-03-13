826274bd1eac97f6b715b3fdbdfd9685a922ebb4
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-13 23:27:34 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,13 +1,11 @@
 def reformat_languages(languages)
-  # your code here
   new_hash = {}
-  blank = []
   languages.each do |keys, values|
-    values.each do |values, types|
+    language.each do |values, types|
       new_hash[values] = types
-      new_hash[values][:style] = []
-      end
-      new_hash[values][:style] = :oo
+      new_languages[language_name][:style] = []
     end
-    new_hash
   end
+  new_languages[:javascript][:style] << :oo
+  new_languages
+end
